f8c0e2241fd350177c8dfe1e27f7587217f7fb7f
Vinícius Viçoso <vinicius_menezes@id.uff.br>
2017-09-05 11:06:51 -0300

    Repetindo para segunda coluna a partir do item 3

--- cmatriz.c
+++ cmatriz.c
@@ -44,7 +44,29 @@ main()
 		
 			M[l][c]=M[l][c]-(lbd*M[0][c]);
 	
-	}     
+	} 
 	
+	
+	//Repetindo para segunda coluna a partir do item 3    
+	   
+    for(l=2;l<4;l++)
+	{
+        lbd=M[l][1]/M[1][1];
+
+		for(c=0;c<5;c++)
+
+			M[l][c]=M[l][c]-(lbd*M[1][c]);
+
+	}  
+	
+	
+	for(l=3;l<4;l++)
+	{
+        	lbd=M[l][2]/M[2][2];
+		for(c=0;c<5;c++)
+
+			M[l][c]=M[l][c]-(lbd*M[2][c]);
+
+	}
      
 }
